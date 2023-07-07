607e006436d7656bcb34d23e62565e2f91e4e680
anujkumar98 <kumar.anuj@northeastern.edu>
2023-07-06 21:19:15 -0400

    Made update in terraform code for changed file

--- terraform/modules/object_storage/main.tf
+++ terraform/modules/object_storage/main.tf
@@ -3,27 +3,15 @@ resource "google_storage_bucket" "safechat_gcs" {
   location      = "US"
   force_destroy = true
 }
-resource "google_storage_bucket_object" "train_data" {
-  name   = "reddit_200k_train.csv"
-  source = "../data/reddit_200k_train.csv"
+resource "google_storage_bucket_object" "safechat_data" {
+  name   = "safechat_data.parquet"
+  source = "../data/cleaned/safechat_data.parquet"
   bucket = google_storage_bucket.safechat_gcs.name
-  content_type = "text/csv"
-}
-resource "google_storage_bucket_object" "test_data" {
-  name   = "reddit_200k_test.csv"
-  source = "../data/reddit_200k_test.csv"
-  bucket = google_storage_bucket.safechat_gcs.name
-  content_type = "text/csv"
+  content_type = "application/octet-stream"
 }
 
-resource "google_storage_object_access_control" "public_rule_train_data" {
-  object = google_storage_bucket_object.train_data.output_name
-  bucket = google_storage_bucket.safechat_gcs.name
-  role   = "READER"
-  entity = "allUsers"
-}
-resource "google_storage_object_access_control" "public_rule_test_data" {
-  object = google_storage_bucket_object.test_data.output_name
+resource "google_storage_object_access_control" "public_rule_data" {
+  object = google_storage_bucket_object.safechat_data.output_name
   bucket = google_storage_bucket.safechat_gcs.name
   role   = "READER"
   entity = "allUsers"
